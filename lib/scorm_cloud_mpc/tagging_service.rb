0fb46030d462f406cfa738cf40c973c5f75ded3f
David Hansen <david.hansen@mediapartners.com>
2019-12-05 20:31:14 -0800

    Add all the tagging service functions

--- lib/scorm_cloud_mpc/tagging_service.rb
+++ lib/scorm_cloud_mpc/tagging_service.rb
@@ -1,8 +1,27 @@
 module ScormCloud
 	class TaggingService < BaseService
-		not_implemented :get_course_tags, :set_course_tags, :add_course_tag,
-				:remove_course_tag, :get_registration_tags, :set_registration_tags,
-				:add_registration_tag, :remove_registration_tag
+
+		def get_course_tags(course_id)
+			xml = connection.call("rustici.tagging.getCourseTags", {:courseid => course_id})
+                        tags = []
+                        xml.elements["/rsp/tags"].elements.each('tag') { |tag| tags << tag.text }
+                        tags
+		end
+
+		def set_course_tags(course_id, tags)
+			xml = connection.call("rustici.tagging.setCourseTags", {:courseid => course_id, :tags => tags})
+			!xml.elements["/rsp/success"].nil?
+		end
+
+		def add_course_tag(course_id, tag)
+			xml = connection.call("rustici.tagging.addCourseTag", {:courseid => course_id, :tag => tag})
+			!xml.elements["/rsp/success"].nil?
+		end
+
+		def remove_course_tag(course_id, tag)
+			xml = connection.call("rustici.tagging.removeCourseTag", {:courseid => course_id, :tag => tag})
+			!xml.elements["/rsp/success"].nil?
+		end
 
 		def get_learner_tags(learner_id)
 			xml = connection.call("rustici.tagging.getLearnerTags", {:learnerid => learner_id})
@@ -23,6 +42,28 @@ module ScormCloud
 
 		def remove_learner_tag(learner_id, tag)
 			xml = connection.call("rustici.tagging.removeLearnerTag", {:learnerid => learner_id, :tag => tag})
+			!xml.elements["/rsp/success"].nil?
+		end
+
+		def get_registration_tags(reg_id)
+			xml = connection.call("rustici.tagging.getRegistrationTags", {:regid => reg_id})
+                        tags = []
+                        xml.elements["/rsp/tags"].elements.each('tag') { |tag| tags << tag.text }
+                        tags
+		end
+
+		def set_registration_tags(reg_id, tags)
+			xml = connection.call("rustici.tagging.setRegistrationTags", {:regid => reg_id, :tags => tags})
+			!xml.elements["/rsp/success"].nil?
+		end
+
+		def add_registration_tag(reg_id, tag)
+			xml = connection.call("rustici.tagging.addRegistrationTag", {:regid => reg_id, :tag => tag})
+			!xml.elements["/rsp/success"].nil?
+		end
+
+		def remove_registration_tag(reg_id, tag)
+			xml = connection.call("rustici.tagging.removeRegistrationTag", {:regid => reg_id, :tag => tag})
 			!xml.elements["/rsp/success"].nil?
 		end
 	end
